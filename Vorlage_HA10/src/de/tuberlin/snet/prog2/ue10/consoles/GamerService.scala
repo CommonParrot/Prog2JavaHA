169456abd8e6023bcb21a91897f4320b5efd152c
SirFailaL0t <jakob-feldmann@outlook.com>
2017-06-26 21:39:50 +0200

    10.1 und 10.2

--- Vorlage_HA10/src/de/tuberlin/snet/prog2/ue10/consoles/GamerService.scala
+++ Vorlage_HA10/src/de/tuberlin/snet/prog2/ue10/consoles/GamerService.scala
@@ -1,20 +1,106 @@
 package de.tuberlin.snet.prog2.ue10.consoles
 
 import java.util.Date
+import scala.collection.mutable.MutableList
+import de.tuberlin.snet.prog2.ue10.consoles.GamerService.GameHolder
 
 object GamerService {
+
   def main(args: Array[String]): Unit = {
-//    val ps = new Console("Sony", "PlayStation 5",
-//      new Date(2017, 8, 23), None,
-//      List(new BD), new UHD4K)
-//    val atari = new Console("Atari", "2600",
-//      new Date(1977, 10, 1), None,
-//      List(new Cartridge), new Entry)
-//    
-//    //...
-//    
-//    
-//    Game("BattleField 0", "EA", List(ps, atari))
-//    
+
+    val ps = new Console("Sony", "PlayStation 5", new Date(2017, 8, 23), None, List(new BlueRay), UHD);
+
+    val atari = new Console("Atari", "2600", new Date(1977, 10, 1), None, List(new Cartridge), HD);
+
+    val switch = new Console("Nintendo", "Switch", new Date(2017, 2, 5), Option.apply("5GHz"), List(new Cartridge), FHD);
+
+    println(switch);
+
+    println(atari);
+
+    println(ps);
+
+    println(List(ps, atari, switch).sortWith((x, y) => x.disp.pixel > y.disp.pixel));
+
+    GameHolder.Game("BattleField 0", "EA", MutableList(ps, atari))
+    GameHolder.Game("BattleField 0", "EA", MutableList(switch))
+
+    GameHolder.Game("BattleField 0", "Ubihard", MutableList(switch))
+    GameHolder.Game("BattleField 0", "Betester", MutableList(switch))
+    println(GameHolder.Game.gamesList);
+
+//    println(new Game("BattleField 0", "Ubihard2", MutableList(switch)))
+
   }
+
+  class Console(var hersteller: String, var model: String, var released: Date, var wifi: Option[String], var formats: List[MediaFormat], var disp: Resolution) {
+
+    override def toString = model //+ " von " + hersteller + " löst auf in " + disp + " und erkennt Spiele auf " + formats;
+
+  }
+
+  class Resolution(x: Int, y: Int, n: String) {
+    var width: Int = x;
+    var height: Int = y;
+    var name: String = n;
+
+    var pixel: Int = width * height;
+
+    override def toString = name;
+  }
+
+  val HD = new Resolution(1280, 720, "HD");
+
+  val FHD = new Resolution(1980, 1280, "FHD");
+
+  val QHD = new Resolution(2560, 1440, "QHD");
+
+  val UHD = new Resolution(3840, 2160, "UHD");
+
+  class MediaFormat(val name: String) {
+    override def toString = name;
+  }
+
+  class Cartridge(name: String = "Cartridge") extends MediaFormat(name) {
+  }
+
+  class DVD(name: String = "DVD") extends MediaFormat(name) {
+  }
+
+  class BlueRay(name: String = "BlueRay") extends MediaFormat(name) {
+  }
+
+  object GameHolder {
+
+    class Game(private val name: String, private val hersteller: String, private val consoles: MutableList[Console]) {
+
+      def isSupported(c: Console): Boolean = {
+        return consoles.contains(c);
+      }
+
+      override def toString = name + " vom Hersteller " + hersteller + " jetzt erhältlich für " + consoles;
+    }
+
+    object Game {
+
+      val gamesList: MutableList[Game] = MutableList();
+
+      def apply(name: String, hersteller: String, consoles: MutableList[Console]) = {
+
+        if (gamesList.toStream.exists(x => x.name == name && x.hersteller == hersteller)) {
+
+          gamesList.toStream.withFilter(x => x.name == name && x.hersteller == hersteller).foreach(x => consoles.foreach(y => if (x.consoles.contains(y)) {} else { x.consoles.+=(y) }));
+
+        } else {
+
+          var newGame: Game = new Game(name, hersteller, consoles);
+          gamesList.+=:(newGame);
+          newGame;
+
+        }
+
+      }
+    }
+  }
+
 }
--- Vorlage_HA10/src/de/tuberlin/snet/prog2/ue10/dataanalyst/CSVMain.java
+++ Vorlage_HA10/src/de/tuberlin/snet/prog2/ue10/dataanalyst/CSVMain.java
@@ -3,6 +3,7 @@ package de.tuberlin.snet.prog2.ue10.dataanalyst;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import de.tuberlin.snet.prog2.ue10.dataanalyst.CSVDataEntry;
@@ -21,7 +22,6 @@ public class CSVMain {
 	 */
 	public static void main(String[] args) {
 		
-		System.out.println("yo");
 		CSVManager csvManager = new CSVManager("resources/EWR_Ortsteile_Berlin_2015.csv");
 		ArrayList<CSVDataEntry> csvData = new ArrayList<CSVDataEntry>();
 		try {
@@ -31,8 +31,12 @@ public class CSVMain {
 		}
 
 		Stream<CSVDataEntry> streamDataset = csvManager.getDatasetStream();
-
 		
+		String bestArea = streamDataset.filter(d -> d.getAltersgruppe().equals("5_10")||d.getAltersgruppe().equals("10_15")||d.getAltersgruppe().equals("15_20")||!d.getStaatsangeh().equals("D"))
+				.collect(Collectors.groupingBy(CSVDataEntry::getOrtsname, Collectors.summarizingDouble(CSVDataEntry::getHauefigkeit))).entrySet().stream()
+				.max((e1,e2) -> e1.getValue().getSum() > e2.getValue().getSum()? 1 : -1).get().getKey();
+		
+		System.out.println(bestArea);
 		
 	}
 }
